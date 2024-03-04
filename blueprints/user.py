5b7441f0aa2baa8ea9a48890c397723572d71b5a
Jackyw225 <zhenjiew@126.com>
2024-03-04 22:37:23 +1100

    9900 login 1

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,7 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('user', __name__, url_prefix='/user')
+
 
 @bp.route('/login')
 def login():
     pass
+
+
+@bp.route('/register')
+def register():
+    return render_template('register.html')
