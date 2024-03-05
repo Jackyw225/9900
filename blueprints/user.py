d21ae2151dabaaf1af9e244afb0c9e46e6f38071
Jackyw225 <zhenjiew@126.com>
2024-03-05 20:23:32 +1100

    邮箱验证码

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
-from exts import mail
+from flask import Blueprint, render_template, jsonify
+from exts import mail, db
 from flask_mail import Message
+from flask import request
+import random
+import string
+from models import UserModel, CaptchaModel
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
@@ -14,12 +18,20 @@ def login():
 def register():
     return render_template('register.html')
 
-@bp.route('/mail')
-def mail_test():
+
+@bp.route('/register/captcha')
+def captcha():
+    email = request.args.get('email')
+    source = string.digits * 6
+    captcha = random.sample(source, 6)
+    captcha = ''.join(captcha)
     message = Message(
-        subject='测试邮件',
-        recipients=['zhenjiew@126.com'],  # 替换为接收者的邮箱地址
-        body='这是一封来自 Flask-Mail 的测试邮件！'
+        subject='Your captcha',
+        recipients=[email],
+        body='This is your captcha: ' + captcha
     )
+    db_captcha = CaptchaModel(email=email, captcha=captcha)
+    db.session.add(db_captcha)
+    db.session.commit()
     mail.send(message)
-    return 'success'
+    return jsonify({'code': 200, 'message': '', 'data': ''})
--- models.py
+++ models.py
@@ -1,6 +1,10 @@
 from exts import db
 from datetime import datetime
 
+# flask db init
+# flask db migrate
+# flask db upgrade
+
 
 class UserModel(db.Model):
     __tablename__ = 'user'
@@ -10,3 +14,10 @@ class UserModel(db.Model):
     email = db.Column(db.String(100), nullable=False, unique=True)
     role = db.Column(db.String(100), nullable=False)
     join_time = db.Column(db.DateTime, default=datetime.now)
+
+class CaptchaModel(db.Model):
+    __tablename__ = 'captcha'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), nullable=False)
+    captcha = db.Column(db.String(100), nullable=False)
+    create_time = db.Column(db.DateTime, default=datetime.now)
